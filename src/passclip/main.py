86fdef62ada962d9624ad51b1aa10402aae7f3c7
samuel-kosmann <samuel.kosmann@gmail.com>
2025-05-25 17:31:40 +0200

    check if word is in word list

--- src/passclip/main.py
+++ src/passclip/main.py
@@ -23,8 +23,17 @@ def generate():
     password = ""
 
     for _ in range(sections):
-        # Generate a random word
-        word = mcg.generate_word(length=section_length)
+        # Generate a random word that is not a real word
+        for attempt in range(11):
+            if attempt == 10:
+                raise ValueError(
+                    "Failed to generate a non-real word after 10 attempts."
+                )
+
+            word = mcg.generate_word(length=section_length)
+            if word in mcg.wordlist:
+                continue
+            break
 
         # Add capitals
         for _ in range(capitals_per_section):
@@ -43,8 +52,7 @@ def generate():
 
     # Copy the generated password to the clipboard
     pyperclip.copy(password)
-    #typer.echo(f"Generated password: {password}")
-
+    # typer.echo(f"Generated password: {password}")
 
 
 if __name__ == "__main__":
